0eefddd182c6a40ec53a02f31310a8666e79caba
Andrew Watson <aww@awwsmm.com>
2021-07-25 19:24:38 +0100

    add selenium browser Google example test

--- tests/Computer.ts
+++ tests/Computer.ts
@@ -1,11 +1,30 @@
 import { expect } from 'chai'
 import Computer from 'src/Computer'
+import { Builder, ThenableWebDriver } from 'selenium-webdriver'
+import 'chromedriver'
 
 describe('Computer', () => {
+
+  let browser: ThenableWebDriver
+
+  before(async () => {
+    browser = new Builder().forBrowser('chrome').build()
+  });
 
   it('should know the answer', () => {
     const computer = new Computer()
     expect(computer.answer).equals(42)
   })
+
+  it('should Google', async function () {
+    this.timeout(10000)
+    await browser.navigate().to('https://www.google.com')
+    const title = await browser.getTitle()
+    expect(title).contains("Google")
+  })
+
+  after(async () => {
+    await browser.close()
+  });
 
 })
