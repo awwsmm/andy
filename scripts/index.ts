a1bfec53e73f3e07830448c952afa331d214c5ac
Andrew Watson <aww@awwsmm.com>
2021-07-25 20:15:50 +0100

    selenium tests to simulate user input and assert results -- I/O integration tests

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,10 +1,29 @@
 import Computer from 'src/Computer'
 import Output from 'src/io/Output'
 
-const output = new Output()
-const computer = new Computer()
+function main() {
+  document.title = "This is the new page title!";
 
-console.log(output.message())
-console.log(`The answer is ${computer.answer}`)
+  const output = new Output()
+  const computer = new Computer()
 
-alert("Hi there!")
+  console.log(output.message())
+  console.log(`The answer is ${computer.answer}`)
+
+  let inputElem = document.createElement('input')
+  inputElem.setAttribute('id', 'theInput')
+  document.body.appendChild(inputElem)
+
+  let buttonElem = document.createElement('button')
+  buttonElem.setAttribute('id', 'theButton')
+  document.body.appendChild(buttonElem)
+
+  buttonElem.onclick = setCapitalTitle
+}
+
+function setCapitalTitle() {
+  let text = (document.getElementById('theInput') as HTMLInputElement).value || "empty title"
+  document.title = text.toUpperCase()
+}
+
+window.onload = main
--- tests/Computer.ts
+++ tests/Computer.ts
@@ -1,14 +1,18 @@
 import { expect } from 'chai'
 import Computer from 'src/Computer'
-import { Builder, ThenableWebDriver } from 'selenium-webdriver'
+import { Builder, By, ThenableWebDriver } from 'selenium-webdriver'
 import 'chromedriver'
+import { Options } from 'selenium-webdriver/chrome'
 
 describe('Computer', () => {
 
   let browser: ThenableWebDriver
 
   before(async () => {
-    browser = new Builder().forBrowser('chrome').build()
+    browser = new Builder().
+      forBrowser('chrome').
+      setChromeOptions(new Options().headless()).
+      build()
   });
 
   it('should know the answer', () => {
@@ -16,11 +20,29 @@ describe('Computer', () => {
     expect(computer.answer).equals(42)
   })
 
+  // see: https://github.com/goenning/typescript-selenium-example
+  // and: https://stackoverflow.com/a/62702647/2925434
   it('should Google', async function () {
     this.timeout(10000)
     await browser.navigate().to('https://www.google.com')
     const title = await browser.getTitle()
     expect(title).contains("Google")
+  })
+
+  it('should know the page title', async function() {
+    const path = __filename.slice(0, __filename.indexOf('/tests'))
+    await browser.navigate().to(`file:///${path}/index.html`)
+    const title = await browser.getTitle()
+    expect(title).contains("This is the new page title!")
+  })
+
+  it('should set THE PAGE TITLE', async function() {
+    const path = __filename.slice(0, __filename.indexOf('/tests'))
+    await browser.navigate().to(`file:///${path}/index.html`)
+    await browser.findElement(By.id('theInput')).sendKeys('buenos dias')
+    await browser.findElement(By.id('theButton')).click()
+    const title = await browser.getTitle()
+    expect(title).contains("BUENOS DIAS")
   })
 
   after(async () => {
